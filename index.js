c6213c91de93c37e021f933ecd22b988b28a0658
Tihomir Godek <tihomirgodek24@gmail.com>
2020-12-22 19:41:53 +0100

    adds documentation for endpoints and implements methods

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express'),
+  bodyParser = require('body-parser'),
   morgan = require('morgan');
 
 const app = express();
 
-let topMovies = [
+app.use(morgan('common'));
+app.use(bodyParser.json());
+
+let movies = [
   {
     title: 'Ace Ventura: Pet Detective',
     actor: 'Jim Carrey',
@@ -46,8 +50,6 @@ let topMovies = [
   },
 ];
 
-app.use(morgan('common'));
-
 app.use(express.static('public'));
 
 // ERROR-HANDLING MIDDLEWARE FUNCTION
@@ -56,13 +58,72 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something is not working!');
 });
 
-// GET requests
-app.get('/', (req, res) => {
-  res.send('Welcome to my Movie app');
+// Return a list of ALL movies to the user
+app.get('/movies', (req, res) => {
+  res.json(movies);
 });
 
-app.get('/movies', (req, res) => {
-  res.json(topMovies);
+// Return data about a single movie by title to the user
+app.get('/movies/:title', (req, res) => {
+  res.json(
+    movies.find((movie) => {
+      return movie.title === req.params.title;
+    })
+  );
+});
+// app.get('/movies/title', (req, res) => {
+//   res.send(
+//     'Successful GET request returning data about a single movie by title to the user'
+//   );
+// });
+
+// Return data about a genre (description) by name/title
+app.get('/movies/genre', (req, res) => {
+  res.send(
+    'Successful GET request returning data about a about a GENRE by name/title'
+  );
+});
+
+// Return data about a director by name
+app.get('/movies/director', (req, res) => {
+  res.send('Successful GET request returning data about a director');
+});
+
+// Allow new users to register
+app.post('/movies/users', (req, res) => {
+  let newUser = req.body;
+
+  if (!newUser.name) {
+    const message = 'Missing name in request body';
+    res.status(400).send(message);
+  } else {
+    movies.push(newUser);
+    res.status(201).send(newUser);
+  }
+});
+
+// Allow users to update thier user info(username)
+app.put('/movies/users/username', (req, res) => {
+  res.send('Successful PUT request letting users change their username');
+});
+
+// Allow users to add a movie to their list of favorites
+app.post('/movies/users/favorites', (req, res) => {
+  res.send(
+    'Successful POST request letting users add a movie to their list of favorites'
+  );
+});
+
+// Allows user to remove a movie from their list of favorites
+app.delete('/movies/users/favorites/list', (req, res) => {
+  res.send(
+    'Successful DELETE request removing a movie from their list of favorites'
+  );
+});
+
+// Allow existing users to deregister
+app.delete('/movies/users/email', (req, res) => {
+  res.send('Successful DELETE request removing email');
 });
 
 // listen for requests
